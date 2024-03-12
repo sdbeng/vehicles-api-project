b7dd760bfd70964e5737ec9c257c1067b37d1de9
Sergio D. Barrera <sdbeng@gmail.com>
2024-03-12 14:30:05 -0700

    add dependencies, update app props

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,13 +1,20 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.PriceRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PricingServiceApplicationTests {
+
+	@Autowired
+	PriceRepository priceRepository;
+
+
 
 	@Test
 	public void contextLoads() {
